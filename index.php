7baef2191a4e37fb38b130b2bf25dcc84b7d7a84
CarloBattista <carlitobatti@gmail.com>
2023-06-01 15:03:46 +0200

    prodotti ciclate dinamichamente

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ class Product
     public $image;
 
     // Constructor
-    public function __construct($id, $title, $price, $category, $type, $image) {
+    public function __construct($id, $title, $price, $category, $type, $image)
+    {
         $this->id = $id;
         $this->title = $title;
         $this->price = $price;
@@ -20,6 +21,30 @@ class Product
         $this->image = $image;
     }
 }
+
+// Classe per categorie
+class Category {
+    public $id;
+    public $name;
+    public $icon;
+
+    // Constructor per categorie
+    public function __construct($id, $name, $icon) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->icon = $icon;
+    }
+}
+
+// Creazione delle categorie
+$dogCategory = new Category(1, "Cani", "fa-solid fa-dog");
+$catCategory = new Category(2, "Gatti", "fa-solid fa-cat");
+
+// Array prodotti
+$products = array(
+    new Product(1, "Pedigree Cibo Secco Cane Manzo e Vedure", 8.49, "Cani", "Cibo", "https://arcaplanet.vtexassets.com/arquivos/ids/273547-200-200/pedigree-adult-manzo-e-verdure-.jpg?v=1771805671"),
+    new Product(2, "Frontline Antiparassitario Spot-On per Gatti 4 Pipette", 19.99, "Gatti", "Cibo", "https://arcaplanet.vtexassets.com/arquivos/ids/252970/Frontline-Spot-On-4-Pipette.jpg?v=637492391040130000"),
+);
 
 ?>
 
@@ -34,7 +59,7 @@ class Product
     <!-- CSS -->
     <link rel="stylesheet" href="./_assets/css/main.css">
     <!-- CDN -->
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css' integrity='sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==' crossorigin='anonymous'/>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css' integrity='sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==' crossorigin='anonymous' />
 </head>
 
 <body>
@@ -49,147 +74,26 @@ class Product
     <div class="main">
         <!-- Container Products -->
         <div class="container_products">
-            <!-- Card -->
+            <!-- Card ciclate in modo dinamico tramite l'array -->
+            <?php foreach ($products as $elem) { ?>
             <div class="card_pr">
                 <div class="wrap_image">
-                    <img class="image_product" src="https://arcaplanet.vtexassets.com/arquivos/ids/269608/advantix-spot-on-25-40-kg.jpg?v=637830163490600000" alt="">
+                    <img class="image_product" src="<?php echo $elem->image ?>" alt="<?php echo $elem->title ?>">
                     <div class="overlay">
                         <div class="box_type_product">
-                            <i class="fa-solid fa-dog"></i>
+                            <!-- Se la categoria è uguale a "Cani" viene aggiunta la classe "fa-dog", se invece la categoria è uguale a "Gatti" viene aggiunta la classe "fa-cat" -->
+                            <i class="fa-solid <?php echo $elem->category === 'Cani' ? 'fa-dog' : ($elem->category === 'Gatti' ? 'fa-cat' : '') ?>"></i>
                         </div>
                     </div>
                 </div>
                 <div class="container_info">
-                    <h2 class="title_product">Titolo prodotto</h2>
-                    <h3 class="price_content">&euro; <span class="price">00,00</span></h3>
-                </div>
-            </div>
-            <div class="card_pr">
-                <div class="wrap_image">
-                    <img class="image_product" src="https://arcaplanet.vtexassets.com/arquivos/ids/269608/advantix-spot-on-25-40-kg.jpg?v=637830163490600000" alt="">
-                    <div class="overlay">
-                        <div class="box_type_product">
-                            <i class="fa-solid fa-dog"></i>
-                        </div>
+                    <h2 class="title_product"><?php echo $elem->title ?></h2>
+                    <div class="primary_line">
+                        <span class="price_content">&euro; <span class="price"><?php echo $elem->price ?></span></span>
                     </div>
                 </div>
-                <div class="container_info">
-                    <h2 class="title_product">Titolo prodotto</h2>
-                    <h3 class="price_content">&euro; <span class="price">00,00</span></h3>
-                </div>
             </div>
-            <div class="card_pr">
-                <div class="wrap_image">
-                    <img class="image_product" src="https://arcaplanet.vtexassets.com/arquivos/ids/269608/advantix-spot-on-25-40-kg.jpg?v=637830163490600000" alt="">
-                    <div class="overlay">
-                        <div class="box_type_product">
-                            <i class="fa-solid fa-dog"></i>
-                        </div>
-                    </div>
-                </div>
-                <div class="container_info">
-                    <h2 class="title_product">Titolo prodotto</h2>
-                    <h3 class="price_content">&euro; <span class="price">00,00</span></h3>
-                </div>
-            </div>
-            <div class="card_pr">
-                <div class="wrap_image">
-                    <img class="image_product" src="https://arcaplanet.vtexassets.com/arquivos/ids/269608/advantix-spot-on-25-40-kg.jpg?v=637830163490600000" alt="">
-                    <div class="overlay">
-                        <div class="box_type_product">
-                            <i class="fa-solid fa-dog"></i>
-                        </div>
-                    </div>
-                </div>
-                <div class="container_info">
-                    <h2 class="title_product">Titolo prodotto</h2>
-                    <h3 class="price_content">&euro; <span class="price">00,00</span></h3>
-                </div>
-            </div>
-            <div class="card_pr">
-                <div class="wrap_image">
-                    <img class="image_product" src="https://arcaplanet.vtexassets.com/arquivos/ids/269608/advantix-spot-on-25-40-kg.jpg?v=637830163490600000" alt="">
-                    <div class="overlay">
-                        <div class="box_type_product">
-                            <i class="fa-solid fa-dog"></i>
-                        </div>
-                    </div>
-                </div>
-                <div class="container_info">
-                    <h2 class="title_product">Titolo prodotto</h2>
-                    <h3 class="price_content">&euro; <span class="price">00,00</span></h3>
-                </div>
-            </div>
-            <div class="card_pr">
-                <div class="wrap_image">
-                    <img class="image_product" src="https://arcaplanet.vtexassets.com/arquivos/ids/269608/advantix-spot-on-25-40-kg.jpg?v=637830163490600000" alt="">
-                    <div class="overlay">
-                        <div class="box_type_product">
-                            <i class="fa-solid fa-dog"></i>
-                        </div>
-                    </div>
-                </div>
-                <div class="container_info">
-                    <h2 class="title_product">Titolo prodotto</h2>
-                    <h3 class="price_content">&euro; <span class="price">00,00</span></h3>
-                </div>
-            </div>
-            <div class="card_pr">
-                <div class="wrap_image">
-                    <img class="image_product" src="https://arcaplanet.vtexassets.com/arquivos/ids/269608/advantix-spot-on-25-40-kg.jpg?v=637830163490600000" alt="">
-                    <div class="overlay">
-                        <div class="box_type_product">
-                            <i class="fa-solid fa-dog"></i>
-                        </div>
-                    </div>
-                </div>
-                <div class="container_info">
-                    <h2 class="title_product">Titolo prodotto</h2>
-                    <h3 class="price_content">&euro; <span class="price">00,00</span></h3>
-                </div>
-            </div>
-            <div class="card_pr">
-                <div class="wrap_image">
-                    <img class="image_product" src="https://arcaplanet.vtexassets.com/arquivos/ids/269608/advantix-spot-on-25-40-kg.jpg?v=637830163490600000" alt="">
-                    <div class="overlay">
-                        <div class="box_type_product">
-                            <i class="fa-solid fa-dog"></i>
-                        </div>
-                    </div>
-                </div>
-                <div class="container_info">
-                    <h2 class="title_product">Titolo prodotto</h2>
-                    <h3 class="price_content">&euro; <span class="price">00,00</span></h3>
-                </div>
-            </div>
-            <div class="card_pr">
-                <div class="wrap_image">
-                    <img class="image_product" src="https://arcaplanet.vtexassets.com/arquivos/ids/269608/advantix-spot-on-25-40-kg.jpg?v=637830163490600000" alt="">
-                    <div class="overlay">
-                        <div class="box_type_product">
-                            <i class="fa-solid fa-dog"></i>
-                        </div>
-                    </div>
-                </div>
-                <div class="container_info">
-                    <h2 class="title_product">Titolo prodotto</h2>
-                    <h3 class="price_content">&euro; <span class="price">00,00</span></h3>
-                </div>
-            </div>
-            <div class="card_pr">
-                <div class="wrap_image">
-                    <img class="image_product" src="https://arcaplanet.vtexassets.com/arquivos/ids/269608/advantix-spot-on-25-40-kg.jpg?v=637830163490600000" alt="">
-                    <div class="overlay">
-                        <div class="box_type_product">
-                            <i class="fa-solid fa-dog"></i>
-                        </div>
-                    </div>
-                </div>
-                <div class="container_info">
-                    <h2 class="title_product">Titolo prodotto</h2>
-                    <h3 class="price_content">&euro; <span class="price">00,00</span></h3>
-                </div>
-            </div>
+            <?php } ?>
         </div>
     </div>
 
